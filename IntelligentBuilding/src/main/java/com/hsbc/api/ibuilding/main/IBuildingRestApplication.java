a9fbbeb70e6f7c8e35abc8e54fceb171a9962dcd
macroE <macroe@163.com>
2018-11-07 23:13:48 +0800

    Update pom to be executable

--- IntelligentBuilding/src/main/java/com/hsbc/api/ibuilding/main/IBuildingRestApplication.java
+++ IntelligentBuilding/src/main/java/com/hsbc/api/ibuilding/main/IBuildingRestApplication.java
@@ -3,7 +3,7 @@ package com.hsbc.api.ibuilding.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (scanBasePackages = { "com.hsbc.api.ibuilding.main", "com.hsbc.api.ibuilding.rest" })
+@SpringBootApplication(scanBasePackages = { "com.hsbc.api.ibuilding.main", "com.hsbc.api.ibuilding.rest" })
 
 public class IBuildingRestApplication {
 	
